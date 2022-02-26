b4fb45cbd92d16c5a36914f5160df926c0b20296
Aaron Faanes <dafrito@gmail.com>
2022-02-26 17:36:11 -0600

    Fix demo

--- src/demo.ts
+++ src/demo.ts
@@ -1,11 +1,11 @@
-import todo from ".";
+import normalizeWheel from ".";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("demo");
   root.style.position = "relative";
 
   const container = document.createElement("div");
-  container.innerHTML = `${todo()}`;
+  container.innerHTML = `${normalizeWheel({deltaX:10, deltaY:10})}`;
   container.style.position = "absolute";
   container.style.left = "0px";
   container.style.top = "0px";
